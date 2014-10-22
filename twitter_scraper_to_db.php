1ae71893a1e34dff8907d047bc5cd4390b059144
Kirk Jantzer <kirk.jantzer@disney.com>
2014-10-22 15:06:00 -0700

    none

--- twitter_scraper_to_db.php
+++ twitter_scraper_to_db.php
@@ -43,7 +43,7 @@ foreach ($results->statuses as $result)
     preg_match("/>([^<]*)</", $result->source, $source_output_array);
     if (!empty($result->geo->coordinates)) {$geo = $result->geo->coordinates[0] . "," . $result->geo->coordinates[1];} else {$geo = "";}
     if (!empty($result->place->bounding_box)) {$geo_bounding_box = $result->place->bounding_box->coordinates[0] . "," . $result->place->bounding_box->coordinates[1] . "," . $result->place->bounding_box->coordinates[2] . "," . $result->place->bounding_box->coordinates[3];} else {$geo_bounding_box = "";}
-    echo $tweet . $geo;
+    echo $geo;
     $sql = "INSERT INTO tweets (tweetid, userid, screenname, name, tweet_geo_coordinates, tweet_place_name, tweet_place_fullname, tweet_place_countrycode, tweet_place_boundingbox_coordinates, tweet_retweets, tweet_favorites, source, location, description, tweet, tweetdate, followers_count, friends_count, statuses_count, favourites_count, listed_count, time_zone, lang) VALUES ('$result->id', '$result->user->id', '$result->user->screen_name', '$result->user->name', $geo, '$result->place->name', '$result->place->full_name', '$result->place->country_code', '$geo_bounding_box', '$result->retweet_count', '$result->favorite_count', '$source_output_array[1]', '$result->user->location', '$result->user->description', $tweet, '$result->created_at', '$result->user->followers_count', '$result->user->friends_count', '$result->user->statuses_count', '$result->user->favourites_count', '$result->user->listed_count', '$result->user->time_zone', '$result->user->lang')";
     echo $sql;
     //mysqli_query($conn, $sql);
