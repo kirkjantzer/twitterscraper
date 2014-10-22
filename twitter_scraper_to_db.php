7258d7acde7b6bd66a3850db5f9f2cef9dd3a54a
Kirk Jantzer <kirk.jantzer@disney.com>
2014-10-22 13:54:59 -0700

    none

--- twitter_scraper_to_db.php
+++ twitter_scraper_to_db.php
@@ -41,7 +41,8 @@ if (!$mysqli->query("SET @a:='this will not work'"))
 
 foreach ($results->statuses as $result) 
 {
-  if (!empty($dbresult = $mysqli->query('SELECT tweetid FROM tweets where `tweetid` = `$result->id`', MYSQLI_USE_RESULT))) 
+  $dbresult = $mysqli->query('SELECT tweetid FROM tweets where `tweetid` = `$result->id`', MYSQLI_USE_RESULT);
+  if (!empty(dbresult)) 
   {
     $tweet = preg_replace(array('/\r/', '/\n/'), '', $result->text);
     preg_match("/>([^<]*)</", $result->source, $source_output_array);
