ef3bc28dad571a73071afebf802fc9b444fc6497
Kirk Jantzer <kirk.jantzer@disney.com>
2014-10-23 09:58:53 -0700

    none

--- twitter_scraper_to_db.php
+++ twitter_scraper_to_db.php
@@ -43,30 +43,31 @@ foreach ($results->statuses as $result)
     preg_match("/>([^<]*)</", $result->source, $source_output_array);
     if (!empty($result->geo->coordinates)) {$geo = $result->geo->coordinates[0] . "," . $result->geo->coordinates[1];} else {$geo = "";}
     if (!empty($result->place->bounding_box)) {$geo_bounding_box = $result->place->bounding_box->coordinates[0][0][0] . "," . $result->place->bounding_box->coordinates[0][0][1] . "," . $result->place->bounding_box->coordinates[0][1][0] . "," . $result->place->bounding_box->coordinates[0][1][1] . "," . $result->place->bounding_box->coordinates[0][2][0] . "," . $result->place->bounding_box->coordinates[0][2][1] . "," . $result->place->bounding_box->coordinates[0][3][0] . "," . $result->place->bounding_box->coordinates[0][3][1];} else {$geo_bounding_box = "";}
-    $result_tweetid = $result->id; 
-    $result_user_id = $result->user->id; 
-    $result_user_screenname = $result->user->screen_name; 
-    $result_user_name = $result->user->name; 
-    $result_geo = $geo; 
-    if (!empty($result->place->name)) {$result_place_name = $result->place->name;} else {$result_place_name = "";}
-    if (!empty($result->place->full_name)) {$result_place_full_name = $result->place->full_name;} else {$result_place_full_name = "";}
-    if (!empty($result->place->country_code)) {$result_place_country_code = $result->place->country_code;} else {$result_place_country_code = "";}
-    if (!empty($result->place->country)) {$result_place_country = $result->place->country;} else {$result_place_country = "";}
-    $result_geo_bounding_box = $geo_bounding_box; 
-    $result_retweet_count = $result->retweet_count; 
-    $result_favorite_count = $result->favorite_count; 
-    $result_source_output_array = $source_output_array[1]; 
-    $result_user_location = $result->user->location; 
-    $result_user_description = $result->user->description; 
-    $result_tweet = $tweet; 
-    $result_created_at = $result->created_at; 
-    $result_user_followers_count = $result->user->followers_count; 
-    $result_user_friends_count = $result->user->friends_count; 
-    $result_user_statuses_count = $result->user->statuses_count; 
-    $result_user_favourites_count = $result->user->favourites_count; 
-    $result_user_listed_count = $result->user->listed_count; 
-    $result_user_time_zone = $result->user->time_zone; 
-    $result_user_lang = $result->user->lang;
+    
+    $result_tweetid = mysqli_real_escape_string($conn, $result->id); 
+    $result_user_id = mysqli_real_escape_string($conn, $result->user->id); 
+    $result_user_screenname = mysqli_real_escape_string($conn, $result->user->screen_name); 
+    $result_user_name = mysqli_real_escape_string($conn, $result->user->name); 
+    $result_geo = mysqli_real_escape_string($conn, $geo); 
+    if (!empty($result->place->name)) {$result_place_name = mysqli_real_escape_string($conn, $result->place->name);} else {$result_place_name = "";}
+    if (!empty($result->place->full_name)) {$result_place_full_name = mysqli_real_escape_string($conn, $result->place->full_name);} else {$result_place_full_name = "";}
+    if (!empty($result->place->country_code)) {$result_place_country_code = mysqli_real_escape_string($conn, $result->place->country_code);} else {$result_place_country_code = "";}
+    if (!empty($result->place->country)) {$result_place_country = mysqli_real_escape_string($conn, $result->place->country);} else {$result_place_country = "";}
+    $result_geo_bounding_box = mysqli_real_escape_string($conn, $geo_bounding_box); 
+    $result_retweet_count = mysqli_real_escape_string($conn, $result->retweet_count); 
+    $result_favorite_count = mysqli_real_escape_string($conn, $result->favorite_count); 
+    $result_source_output_array = mysqli_real_escape_string($conn, $source_output_array[1]); 
+    $result_user_location = mysqli_real_escape_string($conn, $result->user->location); 
+    $result_user_description = mysqli_real_escape_string($conn, $result->user->description); 
+    $result_tweet = mysqli_real_escape_string($conn, $tweet); 
+    $result_created_at = mysqli_real_escape_string($conn, $result->created_at); 
+    $result_user_followers_count = mysqli_real_escape_string($conn, $result->user->followers_count); 
+    $result_user_friends_count = mysqli_real_escape_string($conn, $result->user->friends_count); 
+    $result_user_statuses_count = mysqli_real_escape_string($conn, $result->user->statuses_count); 
+    $result_user_favourites_count = mysqli_real_escape_string($conn, $result->user->favourites_count); 
+    $result_user_listed_count = mysqli_real_escape_string($conn, $result->user->listed_count); 
+    $result_user_time_zone = mysqli_real_escape_string($conn, $result->user->time_zone); 
+    $result_user_lang = mysqli_real_escape_string($conn, $result->user->lang);
     $sql_insert = "INSERT INTO tweets (tweetid, userid, screenname, name, tweet_geo_coordinates, tweet_place_name, tweet_place_fullname, tweet_place_countrycode, tweet_place_country, tweet_place_boundingbox_coordinates, tweet_retweets, tweet_favorites, source, location, description, tweet, tweetdate, followers_count, friends_count, statuses_count, favourites_count, listed_count, time_zone, lang) VALUES ($result_tweetid, $result_user_id, $result_user_screenname, $result_user_name, $result_geo, $result_place_name, $result_place_full_name, $result_place_country_code, $result_place_country, $result_geo_bounding_box, $result_retweet_count, $result_favorite_count, $result_source_output_array, $result_user_location, $result_user_description, $result_tweet, $result_created_at, $result_user_followers_count, $result_user_friends_count, $result_user_statuses_count, $result_user_favourites_count, $result_user_listed_count, $result_user_time_zone, $result_user_lang)";
     echo $sql_insert;
     //mysqli_query($conn, $sql_insert);
