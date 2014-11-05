f55359a44ef134cb95da6da2559ef8aaebba9625
Kirk Jantzer <kirk.jantzer@disney.com>
2014-11-05 10:41:30 -0800

    none

--- tweet_sentiment.php
+++ tweet_sentiment.php
@@ -11,15 +11,13 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql_checkfortweet = "SELECT * FROM tweets where tweet_sentiment IS NULL";
+$sql_checkfortweet = "SELECT * FROM tweets where tweet_sentiment IS NULL limit 30";
 $dbresult = mysqli_query($conn, $sql_checkfortweet);
 $num_dbresult = mysqli_num_rows($dbresult);
-if ($num_dbresult > 0) 
-{
-  $i = 0;
-  while ($i < 31)
-  {
-    foreach ($dbresult as $row)
+
+if (mysqli_num_rows($dbresult) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) 
     {
       $text = $row->tweet;
       $id = $row->id;
@@ -34,9 +32,10 @@ if ($num_dbresult > 0)
       // $sql_insert = "INSERT INTO tweets (tweet_sentiment, tweet_sentimentscore) VALUES ('$tweet_sentiment', '$tweet_sentimentscore') WHERE ID = $id";
       //mysqli_query($conn, $sql_insert) or die(mysqli_error($conn));
     }
-    $i++;
-  }
+} else {
+    echo "0 results";
 }
+
 // Close connection
 mysqli_close($conn);
 ?>
